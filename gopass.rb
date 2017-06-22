48672c4ccedb54e0758de527653f4df8731e1519
Dominik Schulz <dominik.schulz@justwatch.com>
2017-06-22 17:25:56 +0200

    Bump to 1.2.0

--- gopass.rb
+++ gopass.rb
@@ -1,58 +1,56 @@
 class Gopass < Formula
   desc "The slightly more awesome Standard Unix Password Manager for Teams."
-  homepage "https://www.justwatch.com/gopass"
-  url "https://www.justwatch.com/gopass/releases/1.2.0/gopass-1.2.0.tar.gz"
-  sha256 "17c283bbae9b2d306cfeb8537e0857e733f78f533319f59b7bfe5ba2e22cbf14"
+  homepage "https://www.justwatch.com/gopass/"
+  url "https://github.com/justwatchcom/gopass/releases/download/v1.2.0/gopass-1.2.0.tar.gz"
+  version "1.2.0"
+  sha256 "47e3f83d1dcdfddcd2ee8b5af84d4d3ce928191ef5e409d32a0c051bb0c8b91d"
   head "https://github.com/justwatchcom/gopass.git"
-
-  revision 1
-
-  option "with-gnupg21", "Depend on gnupg21 instead of gnupg2"
-  option "with-gnupg1", "Depend on gnupg1 instead of gnupg2"
-
+  
   depends_on "go" => :build
-
+  
+  depends_on "git"
+  option "with-gnupg21", "Depend on gnupg21 instead of gnupg2"
+  option "with-gnupg1", "Depend on gnupg1 instead of gnupg2" 
   depends_on "gnupg2" => :run if build.without?("gnupg1") && build.without?("gnupg21")
   depends_on "gnupg" => :run if build.with?("gnupg1")
   depends_on "gnupg21" => :run if build.with?("gnupg21")
+  
 
   def install
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/justwatchcom/gopass").install buildpath.children
-
+    
     cd "src/github.com/justwatchcom/gopass" do
       ENV["PREFIX"] = prefix
       system "make", "install"
     end
-
+    
     system bin/"gopass completion bash > bash_completion.bash"
     system bin/"gopass completion zsh > zsh_completion.zsh"
     bash_completion.install "bash_completion.bash"
     zsh_completion.install "zsh_completion.zsh"
+    
   end
 
   def caveats; <<-EOS.undent
     Gopass has been installed, have fun!
-
     If upgrading from `pass`, everything should work as expected.
-
     If installing from scratch, you need to either initialize a new repository now...
       gopass init
-
     ...or clone one from a source:
       gopass clone git@code.example.com:example/pass.git
-
     In order to use the great autocompletion features (they're helpful with gopass),
     please make sure you have autocompletion for homebrew enabled:
       https://github.com/bobthecow/git-flow-completion/wiki/Install-Bash-git-completion
-
     More information:
       https://www.justwatch.com/gopass
       https://github.com/justwatchcom/gopass/README.md
-    EOS
+    
+  EOS
   end
-
   test do
     assert_match version.to_s, shell_output("#{bin}/gopass version")
+    
   end
+
 end
